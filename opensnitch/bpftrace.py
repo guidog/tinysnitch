908a7c85960c393f8ffccf410ed1473d832f5c0a
nathants <me@nathants.com>
2019-03-18 11:02:10 -0700

    hacking

--- opensnitch/bpftrace.py
+++ opensnitch/bpftrace.py
@@ -28,6 +28,7 @@ import signal
 
 seconds = 15
 pids = {}
+paths = {}
 
 def _exceptions_kill_parent(decoratee):
     pid = os.getpid()
@@ -56,7 +57,7 @@ def _monitor(proc):
     logging.error('_monitor exited prematurely')
     sys.exit(1)
 
-def _tail(proc):
+def _tail_tcp_udp(proc):
     while True:
         line = proc.stdout.readline().decode('utf-8').rstrip()
         if not line:
@@ -91,67 +92,94 @@ def _gc():
 
 def start():
     run_thread(_gc)
-    for trace in ['tcp', 'udp']:
+    for trace, tail in [('tcp', _tail_tcp_udp), ('udp', _tail_tcp_udp)]:
         proc = subprocess.Popen(['sudo', 'stdbuf', '-o0', f'opensnitch-bpftrace-{trace}'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
         run_thread(_monitor, proc)
-        run_thread(_tail, proc)
+        run_thread(tail, proc)
         logging.info(f'started trace: {trace}')
 
-# def tail_execve(proc):
-#     while True:
-#         line = proc.stdout.readline().decode('utf-8')
-#         if not line:
-#             break
-#         try:
-#             token, line = line.rstrip().split(': ', 1)
-#         except ValueError:
-#             logging.info(f'trace execve skipping: {line.rstrip()}')
-#         else:
+# def tail():
+#     with open(events_pipe_file, 'rb') as pipe:
+#         for line in pipe:
+#             parts = line.split()
 #             try:
-#                 pid, path_and_args = line.split(None, 1)
+#                 name_pid, _, _, _, probe, *rest = parts
 #             except ValueError:
-#                 logging.error(f'bad execve line: {[line]}')
-#             else:
-#                 paths[pid] = path_and_args
-#                 # logging.info(f'execve: {line}')
-#     logging.error('tail exited prematurely')
-#     sys.exit(1)
+#                 logging.error(f'not enough parts: {line}')
+#                 continue
+#             try:
+#                 pid = name_pid.split(b'-')[-1].decode('utf-8')
+#                 if b'opensnitch_exec_probe:' == probe:
+#                     arg_string = [b' ']
+#                     comm = rest[1].split(b'=')[-1].replace(b'"', b'').decode('utf-8')
+#                     for r in rest[2:]:
+#                         r = r.split(b'=')[-1]
+#                         if r == b'(fault)':
+#                             break
+#                         arg_string.append(r)
+#                     arg_string = b' '.join(arg_string).replace(b'"', b'').decode('utf-8')
+#                     comms[pid] = (comm, arg_string)
+#                 elif b'sched_process_exit:' == probe:
+#                     comms.pop(pid, None)
+#                     filenames.pop(pid, None)
+#                 elif b'sched_process_exec:' == probe:
+#                     filename, _pid, _old_pid = rest
+#                     filename = filename.split(b'=')[-1].decode('utf-8')
+#                     filenames[pid] = filename
+#                 elif b'sched_process_fork:' == probe:
+#                     comm, _pid, _child_comm, child_pid = rest
+#                     comm = comm.split(b'=')[-1].decode('utf-8')
+#                     child_pid = child_pid.split(b'=')[-1].decode('utf-8')
+#                     try:
+#                         comms[child_pid] = comms[pid]
+#                     except KeyError:
+#                         comms[child_pid] = comm, ''
+#                     try:
+#                         filenames[child_pid] = filenames[pid]
+#                     except KeyError:
+#                         pass
+#             except UnicodeDecodeError:
+#                 logging.error(f'failed to utf-8 decode kprobe line: {line}')
 
-# def tail_exec(proc):
-#     while True:
-#         line = proc.stdout.readline().decode('utf-8')
-#         if not line:
-#             break
-#         try:
-#             token, line = line.rstrip().split(': ', 1)
-#         except ValueError:
-#             logging.info(f'trace fork skipping: {line.rstrip()}')
-#         else:
-#             try:
-#                 pid, path = line.split(None, 1)
-#             except ValueError:
-#                 logging.error(f'bad fork line: {[line]}')
-#             else:
-#                 if pid not in paths:
-#                     paths[pid] = path
-#     logging.error('tail exited prematurely')
-#     sys.exit(1)
+def tail_execve(proc):
+    while True:
+        line = proc.stdout.readline().rstrip().decode('utf-8')
+        if not line:
+            break
+        try:
+            pid, path, *args = line.split()
+        except ValueError:
+            logging.error(f'bad execve line: {[line]}')
+        else:
+            paths[pid] = (path, args)
+            logging.info(f'execve: {line}')
+    logging.error('tail execve prematurely')
+    sys.exit(1)
 
-# def tail_exit(proc):
-#     while True:
-#         line = proc.stdout.readline().decode('utf-8')
-#         if not line:
-#             break
-#         try:
-#             token, line = line.rstrip().split(': ', 1)
-#         except ValueError:
-#             logging.info(f'trace exit skipping: {line.rstrip()}')
-#         else:
-#             try:
-#                 pid, path = line.split(None, 1)
-#             except ValueError:
-#                 logging.error(f'bad exit line: {[line]}')
-#             else:
-#                 paths.pop(pid, None)
-#     logging.error('tail exited prematurely')
-#     sys.exit(1)
+def tail_exit(proc):
+    while True:
+        line = proc.stdout.readline().decode('utf-8')
+        if not line:
+            break
+        try:
+            pid, path = line.split(None, 1)
+        except ValueError:
+            logging.error(f'bad exit line: {[line]}')
+        else:
+            paths.pop(pid, None)
+    logging.error('tail exited prematurely')
+    sys.exit(1)
+
+def tail_fork(proc):
+    while True:
+        line = proc.stdout.readline().decode('utf-8')
+        if not line:
+            break
+        try:
+            pid, path = line.split(None, 1)
+        except ValueError:
+            logging.error(f'bad fork line: {[line]}')
+        else:
+            ...
+    logging.error('tail fork prematurely')
+    sys.exit(1)
